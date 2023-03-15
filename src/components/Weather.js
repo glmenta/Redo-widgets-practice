29fbd01a5fd58a5748119f5576a1e166b2885120
Gek <gakidogeck@gmail.com>
2023-03-14 20:27:25 -0700

    phase4

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,7 +5,7 @@ function Weather () {
   const [weather, setWeather] = useState(null);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
+    navigator.geolocation?.getCurrentPosition(
       (location) => {
         const fetchWeather = async () => {
           let url = 'http://api.openweathermap.org/data/2.5/weather?';
@@ -108,37 +108,3 @@ export default Weather;
 //         alert ("Check Weather API key!")
 //       }
 //     }
-
-//   render() {
-//     const weather = this.state.weather;
-//     let content = <div className='loading'>loading weather...</div>;
-
-//     if (weather) {
-//       const temp = (weather.main.temp - 273.15) * 1.8 + 32;
-//       content = (
-//         <div>
-//           <p>{weather.name}</p>
-//           <p>{temp.toFixed(1)} degrees</p>
-//         </div>
-//       );
-//     }
-//     else {
-//       content = (
-//         <div>
-//           Weather is currently unavailable. (Are Location Services enabled?)
-//         </div>
-//       )
-//     }
-
-//     return (
-//       <section className="weather-section">
-//         <h1>Weather</h1>
-//         <div className='weather'>
-//           {content}
-//         </div>
-//       </section>
-//     );
-//   }
-// }
-
-// export default Weather;
